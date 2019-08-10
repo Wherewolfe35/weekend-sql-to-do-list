94197ec08a02b0fe9bd94488f9840d82b6c8e477
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-08-10 08:33:03 -0500

    adds client side GET function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,25 @@ $(document).ready(readyOn);
 
 function readyOn() {
   console.log('jQuery is ready');
+  getList();
+}
 
+function getList() {
+  console.log('getting list from server');
+  $.ajax({
+    method: 'GET',
+    url: '/items'
+  }).then((response) => {
+    console.log('List received', response);
+    renderList(response);
+  })
+}
+
+function renderList(listArray) {
+  console.log('rendering list');
+  for (const item of listArray) {
+    let newItem = $(`<li>${item.item}  ${item.completed}</li>`);
+    newItem.data('id', item.id);
+    $('#toDoList').append(newItem);
+  }
 }
